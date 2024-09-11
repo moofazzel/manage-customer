38a2b06164e4a816a52530596135d1dc062bbc57
moofazzel <mofazzel444.com>
2024-09-12 04:06:39 +0600

    temp ignore typescript

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
 import Provider from "@/providers/Provider";
 
-import Header from "@/components/Navbar";
 import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,7 +23,26 @@ export default async function RootLayout({
       <body className={inter.className}>
         <Provider>
           <SessionProvider>
-            <Header />
+            <nav className="container flex justify-between gap-10 pt-5 pb-14">
+              <h2 className="text-3xl font-bold ">Gelman WIFI</h2>
+
+              <div className="space-x-3">
+                <Link
+                  href={"/register"}
+                  className="bg-blue-600  text-white font-bold py-2 px-4 rounded-xl"
+                  type="submit"
+                >
+                  Register
+                </Link>
+                <Link
+                  href={"/login"}
+                  className="bg-blue-600  text-white font-bold py-2 px-4 rounded-xl"
+                  type="button"
+                >
+                  Login
+                </Link>
+              </div>
+            </nav>
             {children}
           </SessionProvider>
         </Provider>
