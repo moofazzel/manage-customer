199e31c1fac082776e7c526f808753c9cb36776b
moofazzel <mofazzel444.com>
2024-09-12 05:21:09 +0600

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,14 @@
+import { Protest_Guerrilla } from "next/font/google";
 import { ServerLogout } from "./ServerLogout";
+
+const protest = Protest_Guerrilla({ weight: "400", subsets: ["latin"] });
 
 function Navbar() {
   return (
     <nav className="container flex justify-between gap-10 pt-5 pb-14">
-      <h2 className="text-3xl font-bold ">Gelman WIFI</h2>
+      <h2 className={`text-3xl font-bold ${protest.className} `}>
+        Gelman WIFI
+      </h2>
 
       <ServerLogout />
     </nav>
